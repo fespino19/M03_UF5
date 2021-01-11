1bf23e53cb8758be63ec51e184fdd12af40ff6bd
jose <jbermejop01@elpuig.xeill.net>
2021-01-11 19:52:49 +0100

    comienzo actividad 2

---
+++ src/A2/Main.java
@@ -0,0 +1,66 @@
+package A2;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String home = System.getProperty("user.home");
+        File file = new File(home+"/"+"santako.txt");
+        FileReader fr = null;
+        String linea = null;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(fr);
+        try {
+            linea = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Pattern PareNoel = Pattern.compile("\\*<]:-DOo");
+        Pattern Rens = Pattern.compile(">:o\\)");
+        Pattern Ajudants = Pattern.compile("[*]<]:-D");
+        int pareVegades = 0;
+        int rensVegades = 0;
+        int ajudantsVegades = 0;
+
+        while (linea != null){
+            System.out.println("Nueva linea");
+            Matcher pare = PareNoel.matcher(linea);
+            Matcher rens = Rens.matcher(linea);
+            Matcher ajudants = Ajudants.matcher(linea);
+
+            if (pare.find()){
+                pareVegades++;
+                System.out.println("Pare Noel"+" ("+pareVegades+")");
+            }
+
+            if (rens.find()){
+                rensVegades++;
+                System.out.println("Rens"+" ("+rensVegades+")");
+            }
+
+            if (ajudants.find()){
+                ajudantsVegades++;
+                System.out.println("Ajudants"+" ("+ajudantsVegades+")");
+            }
+
+            try {
+                linea=br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+    }
+}
